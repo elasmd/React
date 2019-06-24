166155652d8f1fe0c70eed8a752e677f1c589a57
Gorpinevici Vlad <wladik131097@gmail.com>
2019-06-24 13:54:13 +0300

    d

---
+++ precache-manifest.0bca5a788827c11aee927741ea1e7fc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc1589c972a7863bac4099e1d8ca06e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94493c9da039e428cdb2",
+    "url": "/static/css/main.866b4e6a.chunk.css"
+  },
+  {
+    "revision": "17aa20afdb4b3263b91a",
+    "url": "/static/js/2.1599f0e2.chunk.js"
+  },
+  {
+    "revision": "94493c9da039e428cdb2",
+    "url": "/static/js/main.87456bef.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
